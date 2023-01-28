50a5b645bc4cb5168ea0b906f27dd87f016b41e6
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-01-28 20:18:33 +0100

    Fix

--- db/migrate/20230128185303_create_users.rb
+++ db/migrate/20230128185303_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
 
       t.string :username
       t.string :password
+      t.string :email
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_185303) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
