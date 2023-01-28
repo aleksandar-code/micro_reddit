285549b087a1e8700ed423b9bc86cc88e5ab10fa
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-01-28 21:03:28 +0100

    Update association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     validates :link, presence: true, length: { maximum: 1000 }, uniqueness: true
+
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     validates :username, presence: true, length: { in: 3..20 }, uniqueness: true
+
+    has_many :posts
 end
