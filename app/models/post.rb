ede1f210ddd7239332cab81bc76fead0531854c1
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-01-28 22:10:31 +0100

    End

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :link, presence: true, length: { maximum: 1000 }, uniqueness: true
 
     belongs_to :user
+    has_many :comments
 end
--- db/migrate/20230128201926_create_comments.rb
+++ db/migrate/20230128201926_create_comments.rb
@@ -3,6 +3,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
 
       t.string :comment
+      t.references :post
+      t.references :user
       t.timestamps
     end
   end
